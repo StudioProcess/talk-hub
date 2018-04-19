48ef7b525c32af59a3c5576f5d1a67965392582d
Martin Leopold <m@martinleopold.com>
2018-04-19 16:57:36 +0200

    Added show/hide post functions

--- main.js
+++ main.js
@@ -4,16 +4,18 @@ $(() => {
   
   // post elements
   let p = $('._mck9w');
-  console.log(p.length + ' posts'); // output number of posts
+  p.addClass('post');
+  let n = p.length;
+  console.log(n + ' posts'); // output number of posts
   
   // Add post number (Oldest = 1)
   p.attr('data-num', (i) => {
-    return p.length - i;
+    return n - i;
   });
   
   p.css('position', 'relative');
   p.append((i) => {
-    return '<div class="post-num" style="position:absolute; bottom:0; right:0; background:black; color:white; padding:0.33rem; display:none;">' + (p.length-i) + '</div>';
+    return '<div class="post-num" style="position:absolute; bottom:0; right:0; background:black; color:white; padding:0.33rem; display:none;">' + (n-i) + '</div>';
   });
   
   $(document).on('keydown', (e) => {
@@ -23,5 +25,19 @@ $(() => {
       else $('.post-num').hide();
     }
   });
+  
+  let showHideTime = 300;
+  
+  function get(i) { return $(`[data-num=${i}]`); }
+  function show(arr) { arr.forEach(i => get(i).show(showHideTime)); }
+  function hide(arr) { arr.forEach(i => get(i).hide(showHideTime)); }
+  
+  function highlight(arr) {
+    
+  }
+  
+  function dim(arr) {
+    
+  }
   
 });
